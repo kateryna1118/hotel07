223be5c28da6de69d8ab6140107e0b423971616e
cas <cas@ubuntu>
2012-08-08 07:06:50 +0300

    Complete rest

--- src/main/java/ua/softserve/hotel/api/Rest.java
+++ src/main/java/ua/softserve/hotel/api/Rest.java
@@ -1,8 +1,13 @@
 package ua.softserve.hotel.api;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
@@ -18,6 +23,11 @@ import ua.softserve.hotel.service.IRoomService;
 import ua.softserve.hotel.service.IUserService;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 @Controller
 @Path("api")
@@ -66,16 +76,63 @@ public class Rest {
 	@Produces("application/json")
 	@Path("service/history")
 	public Response getServiceHistory() {
-		String result = new Gson().toJson(iAddServiceHistoryService.getAllAddServiceHistories());
+		GsonBuilder gsonBuilder = new GsonBuilder();
+	    Gson gson = gsonBuilder.registerTypeAdapter(AddServiceHistory.class, new AddServiceHistoryAdapter()).create();
+	    String result = gson.toJson(iAddServiceHistoryService.getAllAddServiceHistories());
+	    return Response.status(200).entity(result).build();
+
+		
+//		String result = new Gson().toJson(iAddServiceHistoryService.getAllAddServiceHistories().get(0));
+//		return Response.status(200).entity(result).build();
+	}
+	
+	class AddServiceHistoryAdapter implements JsonSerializer<AddServiceHistory> {
+	    @Override
+	    public JsonElement serialize(AddServiceHistory service, Type type, JsonSerializationContext jsc) {	    	
+	        JsonObject jsonObject = new JsonObject();
+	        jsonObject.addProperty("message_id", service.getAmount());
+	        jsonObject.addProperty("message", service.getDatePrice());
+	        jsonObject.addProperty("id", service.getId());
+	        jsonObject.add("addService", new Gson().toJsonTree(service.getAddService()));
+//	        jsonObject.addProperty("message_id", service.getOpperationDate());
+//	        jsonObject.addProperty("order", String.valueOf(null));
+//	        jsonObject.addProperty("user", service.getUser());
+	        return jsonObject;      
+	    }
+	}
+	
+	@GET
+	@Produces("application/json")
+	@Path("service/history/{role}")
+	public Response getServiceHistory(@PathParam("role") String role) {
+		String result;
+		List<AddServiceHistory> services = new ArrayList<AddServiceHistory>();
+		
+		for(AddServiceHistory service :
+			iAddServiceHistoryService.getAllAddServiceHistories()) {
+			
+			if(service.getUser().getRole().getName().equals(role) &&
+					service.isClientPaid() == null) {
+				services.add(service);
+			}
+		}
+		
+		GsonBuilder gsonBuilder = new GsonBuilder();
+	    Gson gson = gsonBuilder.registerTypeAdapter(AddServiceHistory.class, new AddServiceHistoryAdapter()).create();
+	    result = gson.toJson(services);
 		return Response.status(200).entity(result).build();
 	}
 	
-	@PUT
+	@POST
 	@Produces("application/json")
 	@Path("service/add")
-	public Response addServiceHistory() {
-		AddServiceHistory newAddService = new Gson().fromJson("", AddServiceHistory.class);
+	public Response addServiceHistory(String jsonString) {
+		AddServiceHistory newAddService = new Gson().fromJson(jsonString, AddServiceHistory.class);
+		
+		System.err.println("!!!!!!!!!!!!!! newAddService:" + new Gson().toJson(newAddService));
+		
 		iAddServiceHistoryService.addAddServiceHistory(newAddService);
+		// TODO save to DB
 		return Response.status(200).build();
 	}
 }
